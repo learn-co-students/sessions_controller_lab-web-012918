96083b47c7dda98664d5d23660085186a9db4e67
brookescarlett <brookeyalof2017@u.northwestern.edu>
2018-02-28 11:29:22 -0500

    Done

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    session[:name] = params[:name]
+
+    if session[:name].nil? || session[:name].empty?
+      redirect_to '/sessions/new'
+    else
+      redirect_to '/'
+    end
+  end
+
+  def destroy
+    session[:name] = params[:name]
+
+    if session[:name] != nil
+      session.delete :name
+    end 
+  end
+end
